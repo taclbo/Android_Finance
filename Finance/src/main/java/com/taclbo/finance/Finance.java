cf4c97a0c732cb88abc9749e1fbc78d396e1f1c9
Justin Wyatt <jstnwyatt@gmail.com>
2013-06-20 21:10:00 -0400

    removed unwanted imports from Finance.java

--- Finance/src/main/java/com/taclbo/finance/Finance.java
+++ Finance/src/main/java/com/taclbo/finance/Finance.java
@@ -16,8 +16,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
